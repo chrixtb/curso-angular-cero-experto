d0f40963b0ce8173e0f81d99950560107938f018
Christian Terrón Barcia <christian.terron06@gmail.com>
2025-02-06 16:09:43 +0100

    Funcionalidad gifs-app

--- 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
+++ 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { GifsService } from './../../../gifs/services/gifs.service';
+
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  public tagsHistory: string[];
+
+
+  constructor(private gifsService: GifsService){ 
+    this.tagsHistory = this.gifsService.tagsHistory; 
+  }
+
+  get tags(): string[]{
+    return this.gifsService.tagsHistory;
+  }
+
+  searchTag(tag: string){
+    console.log(`Seleccionado tag: ${tag}`)
+    this.gifsService.searchTag(tag);
+  }
 
 }
