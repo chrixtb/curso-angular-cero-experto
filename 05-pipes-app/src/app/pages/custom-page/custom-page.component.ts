a31e34b4b0816b24cf3198f797d2f329d716a301
Christian Terrón Barcia <christian.terron06@gmail.com>
2025-04-20 02:16:30 +0200

    Inicio custom pipes

--- 05-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 05-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
+import { CanflyPipe } from '../../pipes/canfly.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe, CanflyPipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+
+    name = signal('Fernando Herrera');
+    upperCase = signal(true);
+
+    heroes = signal(heroes);
+
+    changeToggle() {
+        this.upperCase.update(value => !value);
+    }
+
+    //---------------------
+
+
+}
