721e452c1132bb13eba4e38c2804fafff3bccb54
Christian Terrón Barcia <christian.terron06@gmail.com>
2025-05-01 21:09:50 +0200

    Configurar estructura y páginas de la aplicación

--- 06-reactive-forms-app/src/app/app.routes.ts
+++ 06-reactive-forms-app/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'reactive',
+        loadChildren: () => import('./reactive/reactive.routes').then(m => m.reactiveRoutes),
+    },
+    {
+        // en este caso no hace falta el uso del then porque tiene el export default    
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes'),
+    },
+    {
+        path: 'country',
+        loadChildren: () => import('./country/country.routes').then(m => m.countryRoutes),
+    },
+    {
+        path: '**',
+        redirectTo: 'reactive',
+    },
+];
