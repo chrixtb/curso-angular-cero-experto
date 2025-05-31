a6ee23a47433a58fd79e7506de40c11bf5b921ed
Christian Terrón Barcia <christian.terron06@gmail.com>
2025-05-31 18:16:14 +0200

    Modificaciones basic page

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -8,11 +8,42 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent { 
-    myForm = new FormGroup({
-        name: new FormControl(''),
-        price: new FormControl(0),
-        inStorage: new FormControl(0),
+    // myForm = new FormGroup({
+    //     name: new FormControl(''),
+    //     price: new FormControl(0),
+    //     inStorage: new FormControl(0),
+    // });
+
+    // Inicializar usando FormBuilder.
+    private fb = inject(FormBuilder);
+
+    myForm : FormGroup = this.fb.group({
+        // ['Valor por defecto', validadores sincronos , Validadores asincronos ]
+        name: ['', [Validators.required, Validators.minLength(3)], /** Validadores asincronos*/ ],
+        price: [0, [Validators.required, Validators.min(10)],],
+        inStorage: [0, [Validators.required, Validators.min(10)],],
     });
 
+    isValidField(fieldName: string) : boolean | null {
+        return !!this.myForm.controls[fieldName].errors //&& this.myForm.controls[fieldName].touched;
+    }
 
+    getFieldError(fieldName: string) : string | null {
+        if( !this.myForm.controls[fieldName].errors) return null;
+
+        const errors = this.myForm.controls[fieldName].errors ?? {};
+
+        for (const key of Object.keys(errors)) {
+            switch (key) {
+                case 'required':
+                    return 'Este campo es requerido';
+                case 'minlength':
+                    return `El campo debe tener al menos ${errors['minlength'].requiredLength} caracteres`;
+                case 'min':
+                    return `El valor mínimo es ${errors['min'].min}`;
+            }
+        }
+        return null;
+    }
+    
 }
